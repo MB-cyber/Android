ca3ef4a880f00c7df9bc78db735c2d81c76a24d3
MB-cyber <mouhssine.bentabet@gmail.com>
2022-03-26 21:57:29 +0100

    image1

---
+++ app/src/main/java/com/example/bobafettbook/Images.java
@@ -0,0 +1,16 @@
+package com.example.bobafettbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Images extends AppCompatActivity {
+
+    ImageView Images;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
